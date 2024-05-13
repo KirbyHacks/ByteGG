acc0055f6d93c8171377e64a455868ba1758ae73
KirbyHacks <kirbywtfofficial@gmail.com>
2024-05-12 23:20:22 -0300

    Commited (The main project)

---
+++ update.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.check_call(["rm", "-rf", "build"])
+subprocess.check_call(["rm", "-rf", "dist"])
+subprocess.check_call(["rm", "-rf", "bytegg.egg-info/"])
+subprocess.check_call(["python", "setup.py", "sdist", "bdist_wheel"])
+subprocess.check_call(["git", "add", "."])
+subprocess.check_call(["git", "commit", "-a"])
+subprocess.check_call(["twine", "upload", "dist/*"])
